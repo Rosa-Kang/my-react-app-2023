3e526933d25343090aac394f31e1be7e275abdd6
Rosa Kang <therosessom@gmail.com>
2023-08-03 19:23:54 -0700

    update branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Education from './components/Education'
+import Experience from './components/Experience'
+import Personal from './components/Personal'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>Hello world</h1>
-    </>
+      <Personal />
+      <Education />
+      <Experience />
+     </>
   )
 }
 
